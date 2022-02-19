b6694c46a6cc41765e8a086fadee9a0dc56afa46
Sweety-23 <alf23sweety@gmail.com>
2022-02-19 17:38:07 +0530

    ff

--- jj.js
+++ jj.js
@@ -21,3 +21,30 @@ for(let i = 0; i<list.length; i++){
     }
     })
 }
+
+const navToggle = document.querySelector("#navToggle");
+const navClosedIcon = document.querySelector("#navClosed");
+const navOpenIcon = document.querySelector("#navOpen");
+const navIcon = document.querySelectorAll(".navIcon");
+const nav = document.querySelector("nav");
+
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  navIcon.forEach((icon) => {
+    icon.classList.toggle("hidden");
+  });
+});
+
+
+window.addEventListener(
+  "resize", () => {
+    if (document.body.clientWidth > 720) {
+      nav.classList.remove("open");
+      navIcon.forEach((icon) => {
+        icon.classList.remove("hidden");
+      });
+      navOpenIcon.classList.add("hidden");
+    }
+  },
+  { passive: false }
+);
